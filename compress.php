a2bd69175f66ab55c556409428d72afbee0354df
Vallabh Kansagara <vallabh.kansagara@knowarth.com>
2017-04-05 19:22:15 +0530

    code updated

--- compress.php
+++ compress.php
@@ -41,127 +41,35 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-
 function getCompressedOutPut($buffer)
 {
-    if (strpos($buffer, '<pre>') !== false) {
-        $replace = array(
-            '/<!--[^\[](.*?)[^\]]-->/s' => '',
-            "/<\?php/" => '<?php ',
-            "/\r/" => '',
-            "/>\n</" => '><',
-            "/>\s+\n</" => '><',
-            "/>\n\s+</" => '><'
-        );
-    } else {
-        $replace = array(
-            '/<!--[^\[](.*?)[^\]]-->/s' => '',
-            "/<\?php/" => '<?php ',
-            "/\n([\S])/" => '$1',
-            "/\r/" => '',
-            "/\n/" => '',
-            "/\t/" => '',
-            "/ +/" => ' '
-        );
-    }
-
-    // Remove html comment;
-    $additionaly = array(
-        // strip whitespaces after tags, except space
-        '/\>[^\S ]+/s' => '>',
-        // strip whitespaces before tags, except space
-        '/[^\S ]+\</s' => '<',
-        // shorten multiple whitespace sequences
-        '/(\s)+/s' => '\\1',
-        // Remove html comment
-        '!/\*.*?\*/!s' => '',
-        '/\n\s*\n/' => ''
-    );
-     //$buffer = preg_replace(array_keys($replace), array_values($replace), $buffer);
-     $buffer = preg_replace('/<!--(.*)-->/Uis', '', $buffer);
+    $additionaly = array('/\>[^\S ]+/s' => '>','/[^\S ]+\</s' => '<','/(\s)+/s' => '\\1','!/\*.*?\*/!s' => '','/\n\s*\n/' => '');
+    $buffer = preg_replace('/<!--(.*)-->/Uis', '', $buffer);
     $buffer = preg_replace(array_keys($additionaly), array_values($additionaly), $buffer);
     ini_set("pcre.recursion_limit", "16777");
-//    ini_set('zlib.output_compression', 'On'); // If you like to enable GZip, too!
     $buffer = compress($buffer);
     return $buffer;
 }
+
 function compress($buffer)
 {
-    /**
-     * To remove useless whitespace from generated HTML, except for Javascript.
-     * [Regex Source]
-     * https://github.com/bcit-ci/codeigniter/wiki/compress-html-output
-     * http://stackoverflow.com/questions/5312349/minifying-final-html-output-using-regular-expressions-with-codeigniter
-     */
-    $regexRemoveWhiteSpace = '%# Collapse ws everywhere but in blacklisted elements.
-        (?>             # Match all whitespaces other than single space.
-          [^\S ]\s*     # Either one [\t\r\n\f\v] and zero or more ws,
-        | \s{2,}        # or two or more consecutive-any-whitespace.
-        ) # Note: The remaining regex consumes no text at all...
-        (?=             # Ensure we are not in a blacklist tag.
-          (?:           # Begin (unnecessary) group.
-            (?:         # Zero or more of...
-              [^<]++    # Either one or more non-"<"
-            | <         # or a < starting a non-blacklist tag.
-              (?!/?(?:textarea|pre)\b)
-            )*+         # (This could be "unroll-the-loop"ified.)
-          )             # End (unnecessary) group.
-          (?:           # Begin alternation group.
-            <           # Either a blacklist start tag.
-            (?>textarea|pre)\b
-          | \z          # or end of file.
-          )             # End alternation group.
-        )  # If we made it here, we are not in a blacklist tag.
-        %ix';
-    $regexRemoveWhiteSpace = '%(?>[^\S ]\s*| \s{2,})(?=(?:(?:[^<]++| <(?!/?(?:textarea|pre)\b))*+)(?:<(?>textarea|pre)\b|\z))%ix';
-    $re = '%# Collapse whitespace everywhere but in blacklisted elements.
-        (?>             # Match all whitespans other than single space.
-          [^\S ]\s*     # Either one [\t\r\n\f\v] and zero or more ws,
-        | \s{2,}        # or two or more consecutive-any-whitespace.
-        ) # Note: The remaining regex consumes no text at all...
-        (?=             # Ensure we are not in a blacklist tag.
-          [^<]*+        # Either zero or more non-"<" {normal*}
-          (?:           # Begin {(special normal*)*} construct
-            <           # or a < starting a non-blacklist tag.
-            (?!/?(?:textarea|pre|script)\b)
-            [^<]*+      # more non-"<" {normal*}
-          )*+           # Finish "unrolling-the-loop"
-          (?:           # Begin alternation group.
-            <           # Either a blacklist start tag.
-            (?>textarea|pre|script)\b
-          | \z          # or end of file.
-          )             # End alternation group.
-        )  # If we made it here, we are not in a blacklist tag.
-        %Six';
+    $regexRemoveWhiteSpace='%(?>[^\S]\s*|\s{2,})(?=(?:(?:[^<]++|<(?!/?(?:textarea|pre)\b))*+)(?:<(?>textarea|pre)\b|\z))%ix';
+    $re = '%(?>[^\S]\s*|\s{2,})(?=[^<]*+(?:<(?!/?(?:textarea|pre|script)\b)[^<]*+)*+(?:<(?>textarea|pre|script)\b|\z))%Six';
 
-    // $new_buffer = preg_replace('/<!--(.*|\n)-->/Uis', " ", sanitize_output($buffer));
-    // $new_buffer = preg_replace('/\s+/', " ", sanitize_output($new_buffer));
-    $new_buffer = preg_replace($regexRemoveWhiteSpace, " ", sanitize_output($buffer));
-
-    // We are going to check if processing has working
+    $new_buffer = preg_replace('/<!--(.*|\n)-->/Uis', " ", sanitize_output($buffer));
+    $new_buffer = preg_replace('/\s+/', " ", sanitize_output($new_buffer));
+    $new_buffer = preg_replace($regexRemoveWhiteSpace, " ", sanitize_output($new_buffer));
+    $new_buffer = preg_replace($re, " ", sanitize_output($new_buffer));
     if ($new_buffer === null) {
         $new_buffer = $buffer;
     }
-
     return $new_buffer;
 }
+
 function sanitize_output($buffer)
 {
-    $search = array(
-        '/\>[^\S ]+/s', // strip whitespaces after tags, except space
-        '/[^\S ]+\</s', // strip whitespaces before tags, except space
-        '/(\s)+/s', // shorten multiple whitespace sequences
-        '!/\*.*?\*/!s', // Remove htmlcomment
-        '/\n\s*\n/'
-    ); // Remove htmlcomment
-
-    $replace = array(
-        '>',
-        '<',
-        '\\1',
-        '',
-        ''
-    );
+    $search = array('/\>[^\S ]+/s','/[^\S ]+\</s','/(\s)+/s','!/\*.*?\*/!s','/\n\s*\n/');
+    $replace = array('>','<','\\1','','');
     $buffer = preg_replace($search, $replace, $buffer);
     return $buffer;
 }
